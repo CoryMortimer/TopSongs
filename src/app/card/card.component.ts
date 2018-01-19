72914ef02e15e4a59b2380696a108862be0b9c85
Cory Mortimer <cory.m.mortimer@gmail.com>
2018-01-18 21:05:45 -0800

    Add Song type in card component
    Add the Song type in the card component to increase readability in the component.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 
+import { Song } from '../data_and_interfaces/song.interface';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-  @Input() songData;
+  @Input() songData: Song;
 }
