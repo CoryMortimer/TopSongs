548546b22f91d96f631f4441257835e740d0ce03
Cory Mortimer <cory.m.mortimer@gmail.com>
2018-01-22 20:31:20 -0800

    Add search functionality to browse page
    Use the value from the sarch component to filter based off of the artist name and the song title.

--- src/app/browse/browse.service.spec.ts
+++ src/app/browse/browse.service.spec.ts
@@ -30,4 +30,22 @@ describe('BrowseService', () => {
     }
     expect(service.getJustAddedIndicies()).toEqual(trendingIndicies);
   });
+
+  it('should return [0] if only the first song matches', () => {
+    const mockSong = {title: 'test'};
+    const result = service.getMatchingSongIndicies('st', <any>[mockSong]);
+    expect(result).toEqual([0]);
+  });
+
+  it('should return [] for a song that doesn\'t match', () => {
+    const mockSong = {title: 'test'};
+    const result = service.getMatchingSongIndicies('tt', <any>[mockSong]);
+    expect(result).toEqual([]);
+  });
+
+  it('should return [1] for a song that matches the artist name', () => {
+    const mockSongs = [{title: 'test'}, {title: 'no', artist: {name: 'MaTcH'}}];
+    const result = service.getMatchingSongIndicies('mAtCh', <any>mockSongs);
+    expect(result).toEqual([1]);
+  });
 });
